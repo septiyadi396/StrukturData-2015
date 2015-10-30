9014de4b30d941e93ccf8717ed0441d9cb87b8e6
Septiyadi Sistya <adekielapan@gmail.com>
2015-10-30 17:26:20 +0700

    Vector3D UnitTest02

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -7,11 +7,22 @@
  */
 public class Vector3D
 {
-    double x, y, z;
+    private double x, y, z;
+    
     
     public Vector3D(double x, double y, double z) {
-    this.x=x;
-    this.y=y;
-    this.z=z;
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+    
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ(){
+        return z;
     }
 }
