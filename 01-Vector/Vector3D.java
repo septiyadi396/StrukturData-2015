5f25dbe30c16a4689d2ec93d3004970d5214505c
Septiyadi Sistya <adekielapan@gmail.com>
2015-10-30 16:36:04 +0700

    Vector3D 30 okto 15

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Vector3D here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vector3D
+{
+    double x, y, z;
+    
+    public Vector3D(double x, double y, double z) {
+    this.x=x;
+    this.y=y;
+    this.z=z;
+    }
+}
